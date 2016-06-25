da5ee1411c2dbfee1481ec8b5713f43a0b2ed00b
nussland <nussland@gmail.com>
2016-06-25 08:54:26 +0000

    first

---
+++ photos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.conf import settings
+
+class Photo(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    image = models.ImageField(upload_to='%Y/%m/%d')
+    desc = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
